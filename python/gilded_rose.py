37bfedbb30f0dda7814d318fb7aec5582c088235
quentinchad <chirat.quentin@gmail.com>
2026-03-04 21:18:49 +0100

    ajout logique après expiration aged brie

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -38,16 +38,9 @@ class GildedRose(object):
             item.sell_in -= 1
            
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+
+                if item.name == "Aged Brie":
+                    self._increase_quality(item)
 
                         
 
